8fbdfbc9521901b3c7edc7486f5dbdf8142242a1
sy <somarl@live.com>
2022-10-18 14:15:10 +0800

    update caret icons

--- src/icons.js
+++ src/icons.js
@@ -3,14 +3,14 @@ import React from "react"
 export function ChevronDown () {
     return (
         <svg
-            className="svg-inline--fa fa-caret-down fa-w-10"
-            viewBox="0 0 320 512"
+            className="svg-inline--fa fa-chevron-down fa-w-10"
+            viewBox="0 0 512 512"
             xmlns="http://www.w3.org/2000/svg"
-            data-icon="caret-down"
+            data-icon="chevron-down"
             data-prefix="fas"
         >
             <path
-                d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
                 fill="currentColor"
             />
         </svg>
@@ -21,13 +21,13 @@ export function ChevronUp () {
     return (
         <svg
             className="svg-inline--fa fa-chevron-up fa-w-14"
-            viewBox="0 0 448 512"
+            viewBox="0 0 512 512"
             xmlns="http://www.w3.org/2000/svg"
             data-icon="chevron-up"
             data-prefix="fas"
         >
             <path
-                d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
+                d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
                 fill="currentColor"
             />
         </svg>
@@ -61,7 +61,7 @@ export function CaretUp () {
             data-prefix="fas"
         >
             <path
-                d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+                d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"
                 fill="currentColor"
             />
         </svg>
